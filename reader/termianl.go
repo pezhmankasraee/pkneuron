deded6de8846489f849fdd2df9be2b6882cb8f8e
pezhman <github@pezhmankasraee.com>
2025-07-10 13:07:01 +0200

    fix bug for big string chunk
    	- input was delayed with big chunk of pasted strings
    	- solution was to use strings.Builder istead of []string

--- reader/termianl.go
+++ reader/termianl.go
@@ -15,7 +15,10 @@ func Read() string {
 	fmt.Println("\033[33;1mNOTE: use c:s / c:send at the end of the request)\033[0m")
 	fmt.Println("\033[33mrequest > \033[0m")
 
-	var lines []string
+	var sb strings.Builder // Use strings.Builder for efficient concatenation
+	// sb.Grow(initialCapacity) // Optional: If you can estimate total size, pre-allocate for even more efficiency
+
+	firstLine := true
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
@@ -23,7 +26,8 @@ func Read() string {
 			break
 		}
 
-		line = strings.TrimRight(line, "\r\n")
+		line = strings.TrimRight(line, "\r\n") // Trim both \r and \n
+
 		if strings.EqualFold(line, "c:e") || strings.EqualFold(line, "c:exit") {
 			fmt.Println("Exiting ...")
 			os.Exit(0)
@@ -33,9 +37,12 @@ func Read() string {
 			break
 		}
 
-		lines = append(lines, line)
+		if !firstLine {
+			sb.WriteString("\n") // Add newline separator *before* appending subsequent lines
+		}
+		sb.WriteString(line)
+		firstLine = false
 	}
-	input := strings.Join(lines, "\n")
 
-	return input
+	return sb.String() // Get the final string from the builder
 }
